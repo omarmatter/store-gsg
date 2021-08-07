f44638ac0cfcd2c1eb56b412cdcff5b55e003362
omarmatter <omabd2001@gmail.com>
2021-08-07 18:55:55 +0300

    middleware

---
+++ app/Http/Middleware/CheckUserType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckUserType
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user =$request->user();
+        if($user->type =='user'){
+            abort(403);
+        };
+        return $next($request);
+    }
+}
